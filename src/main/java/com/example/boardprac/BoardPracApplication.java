f5cc2f0b9c7979fba9868684b9304670e58e3314
YuJin Kwon <tokyj515@gmail.com>
2022-10-29 20:31:09 +0900

    connect database, setting

--- src/main/java/com/example/boardprac/BoardPracApplication.java
+++ src/main/java/com/example/boardprac/BoardPracApplication.java
@@ -2,8 +2,11 @@ package com.example.boardprac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+
 public class BoardPracApplication {
 
     public static void main(String[] args) {

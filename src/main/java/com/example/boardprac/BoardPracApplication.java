2afaa3ad7b57fb3f972f6b8c5c6d111f44aa7c47
YuJin Kwon <tokyj515@gmail.com>
2022-10-29 16:59:26 +0900

    initialize

---
+++ src/main/java/com/example/boardprac/BoardPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boardprac/BoardPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
